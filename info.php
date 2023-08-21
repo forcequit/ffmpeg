182e2951b0ebd06a8c2d730871f19d110c6ab222
Chris Byrne <chris.byrne@gmail.com>
2023-08-21 22:11:54 +0100

    Update info.php

--- info.php
+++ info.php
@@ -1,5 +1,15 @@
 <?PHP
 
 echo 'Hello World';
+if (extension_loaded('ffmpeg')) {
+    echo "FFmpeg PHP extension is loaded!<br>";
+
+    // Get the ffmpeg version
+    $ffmpegInstance = new ffmpeg_movie('path_to_some_video_file.mp4'); // Replace with a path to an actual video file on your server
+    echo "FFmpeg version: " . $ffmpegInstance->getFFmpegVersion();
+} else {
+    echo "FFmpeg PHP extension is not loaded.";
+}
+
 
 ?>
