4767d52b5373b4c49629e904c7786cbee2954702
Chris Byrne <chris.byrne@gmail.com>
2023-08-22 00:16:52 +0100

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -1,18 +1,14 @@
 <?PHP
 
-$directory = '/home/forge/default/vendor/php-ffmpeg/php-ffmpeg';  // Adjust the path if necessary
-$contents = shell_exec("ls {$directory}");
-echo nl2br($contents);
-
-exit;
 
 require 'vendor/autoload.php';
 use FFMpeg\FFMpeg;
 use FFMpeg\Format\Audio\Mp3;
-use FFMpeg\FFProbe;
 
 // Create an FFMpeg instance
 $ffmpeg = FFMpeg::create();
+
+exit;
 
 // Open the audio file
 $audio = $ffmpeg->open('test.mp3');
