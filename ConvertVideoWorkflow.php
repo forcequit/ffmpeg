5523213a5620ff1eea926046d941da6f795976f9
Chris Byrne <chris.byrne@gmail.com>
2023-08-21 22:02:58 +0100

    Create ConvertVideoWorkflow.php

---
+++ ConvertVideoWorkflow.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Workflows\ConvertVideo;
+
+use FFMpeg\FFMpeg;
+use FFMpeg\Format\Video\WebM;
+use Workflow\Activity;
+
+class ConvertVideoWebmActivity extends Activity
+{
+    public $timeout = 5;
+
+    public function execute($input, $output)
+    {
+        $ffmpeg = FFMpeg::create();
+        $video = $ffmpeg->open($input);
+        $format = new WebM();
+        $format->on('progress', fn () => $this->heartbeat());
+        $video->save($format, $output);
+    }
+}
