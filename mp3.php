4727ba7b675b870d63e80b5df78e85afaefc2bd5
Chris Byrne <chris.byrne@gmail.com>
2023-08-22 20:20:15 +0100

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -1,8 +1,20 @@
 <?PHP
 
 
-$output = shell_exec('find /home/forge/default/vendor/php-ffmpeg -name ffmpeg');
-echo "<pre>$output</pre>";
+// Define the commands
+$commands = [
+    "cd ffmpeg-5.1.2",
+    "./configure --enable-gpl --enable-libmp3lame --enable-libopus --enable-libvpx --enable-libx264 --enable-libx265 --enable-nonfree",
+    "make",
+    "sudo make install"
+];
+
+// Execute the commands
+foreach ($commands as $command) {
+    $output = shell_exec($command . " 2>&1");
+    echo "<pre>$command\n$output</pre>";
+}
+
 
 exit;
 
