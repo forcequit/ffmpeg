ac3726e15ada4f4c2adfa9066c7f482461035723
Chris Byrne <chris.byrne@gmail.com>
2023-08-23 12:21:07 +0100

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -10,18 +10,16 @@ use FFMpeg\Coordinate\TimeCode;
 $ffmpeg = FFMpeg::create(array(
 ));
 
-exit;
-
 
 // Open the audio file
-$audio = $ffmpeg->open('test.mp3');
+$audio = $ffmpeg->open('vo.mp3');
 
 // Set the desired format (MP3) and specify it should be mono (1 channel)
 $format = new Mp3();
 $format->setAudioChannels(1);
 
 // Save the converted file
-$audio->save($format, 'mono.mp3');
+$audio->save($format, 'vo-mono.mp3');
 
 echo "Conversion completed!";
 
