00d0ec53b86a064e5c00b58941aff66abfb45faf
Chris Byrne <chris.byrne@gmail.com>
2023-08-23 13:08:45 +0100

    Update mp3.php

--- mp3.php
+++ mp3.php
@@ -6,11 +6,9 @@ use FFMpeg\FFMpeg;
 use FFMpeg\Coordinate\TimeCode; //not used.
 use FFMpeg\Format\Audio\Mp3;
 
-
 // Create an FFMpeg instance
 $ffmpeg = FFMpeg::create(array(
 ));
-
 
 // Open the audio file
 $audio = $ffmpeg->open('vo.mp3');
@@ -18,7 +16,7 @@ $audio = $ffmpeg->open('vo.mp3');
 $format = new Mp3();
 $format->setAudioChannels(1); //mono
 $format->setAudioKiloBitrate(16); // Lower bitrate
-$format->addFilter('-q:a', 7);
+$format->addFilter(['-qscale', 7]);
 
 $metadata = $audio->getFormat()->get('tags');
 
@@ -26,6 +24,5 @@ $metadata = $audio->getFormat()->get('tags');
 $audio->save($format, 'vo-mono.mp3');
 
 echo 'Completed: ' . $metadata;
-
 
 ?>
