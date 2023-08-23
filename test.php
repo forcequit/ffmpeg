5380a171ce38be34510e1cba5ee9dbf931c14517
Chris Byrne <chris.byrne@gmail.com>
2023-08-23 14:54:10 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,5 @@
 <?php
 
-namespace App\Workflows\ConvertVideo;
-
-use Workflow\ActivityStub;
-use Workflow\Workflow;
-
-class ConvertVideoWorkflow extends Workflow
-{
-    public function execute()
-    {
-        yield ActivityStub::make(
-            ConvertVideoWebmActivity::class,
-            storage_path('/timur.mp4'),
-            storage_path('/timur.webm'),
-        );
-    }
-}
+echo 'hello world';
 
 ?>
